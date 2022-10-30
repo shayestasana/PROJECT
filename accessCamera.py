882cdc62a77ae08ada252408e1bbad9f744fc6a1
shayestasana <108970819+shayestasana@users.noreply.github.com>
2022-10-30 23:40:13 +0530

    tik tok

--- accessCamera.py
+++ accessCamera.py
@@ -5,7 +5,8 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 def distanceCalculate(p1, p2):
-    return ((p1.x - p2.x)**2 + (p1.y - p2.y)**2)**0.5
+    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return dis
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
@@ -20,7 +21,8 @@ with mp_hands.Hands(
             print("Ignoring empty camera frame.")
             # If loading a video, use 'break' instead of 'continue'.
             continue
-        w, h, _ = image.shape
+        h, w, _ = image.shape
+        image = cv2.flip(image, 1)
         # To improve performance, optionally mark the image as not writeable to
         # pass by reference.
         image.flags.writeable = False
@@ -32,19 +34,29 @@ with mp_hands.Hands(
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
-                #   mp_drawing.draw_landmarks(
-                #       image,
-                #       hand_landmarks,
-                #       mp_hands.HAND_CONNECTIONS,
-                #       mp_drawing_styles.get_default_hand_landmarks_style(),
-                #       mp_drawing_styles.get_default_hand_connections_style())
-                coords_1 = mp_drawing._normalized_to_pixel_coordinates(hand_landmarks.landmark[8].x, hand_landmarks.landmark[8].y, w, h)
-                coords_2 = mp_drawing._normalized_to_pixel_coordinates(hand_landmarks.landmark[12].x, hand_landmarks.landmark[12].y, w, h)
-                cv2.circle(image, coords_1, 10, (0, 255, 255), -1)
-                cv2.circle(image, coords_2, 10, (255, 100, 255), -1)
-                distance = distanceCalculate(coords_1, coords_2)
-                cv2.putText(image, str(distance), (int(coords_1[0]), int(coords_1[1] - 20)), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-            cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
+                coords_1 = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP]
+                coords_2 = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP]
+                try:
+                    # normalize the coordinates
+                    x1, y1 = int(coords_1.x * w), int(coords_1.y * h)
+                    x2, y2 = int(coords_2.x * w), int(coords_2.y * h)
+                    cv2.circle(image, (x1, y1), 5, (255, 0, 0), -1)
+                    cv2.circle(image, (x2, y2), 5, (0, 255, 0), -1)
+                    # calculate the distance
+                    dis = distanceCalculate((x1, y1), (x2, y2))
+                    # draw the distance at the bottom of the image
+                    cv2.putText(image, str(int(dis)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+                    # logic 
+                    if dis < 30:
+                        # display joined
+                        cv2.putText(image, "Selector", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+                    else:
+                        # display not joined
+                        cv2.putText(image, "Pointer", (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+                except Exception as e:
+                    print(e)
+        # display quiz ui
+        cv2.imshow('MediaPipe Hands', image)
         if cv2.waitKey(5) & 0xFF == 27:
             break
 cap.release()
